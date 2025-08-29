2a1c2b8c2f01c7c976550b315842f7a47daf00c7
sahil choudhury <sahil.choudhury104@gmail.com>
2025-08-29 15:23:44 +0530

    Update trapping_rainwater.c

--- trapping_rainwater.c
+++ trapping_rainwater.c
@@ -1,1 +1,26 @@
+#include <stdio.h>
+
+int trapRainWater(int a[], int n) {
+    int left = 0, right = n - 1, lmax = 0, rmax = 0, water = 0;
+
+    while (left < right) {
+        if (a[left] < a[right]) {
+            if (a[left] >= lmax) lmax = a[left];
+            else water += lmax - a[left];
+            left++;
+        } else {
+            if (a[right] >= rmax) rmax = a[right];
+            else water += rmax - a[right];
+            right--;
+        }
+    }
+    return water;
+}
+
+int main() {
+    int arr[] = {3, 0, 2, 0, 4};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Total trapped rainwater = %d\n", trapRainWater(arr, n));
+    return 0;
+}
 
